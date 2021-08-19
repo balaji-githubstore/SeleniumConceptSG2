dad213c4d0778f29d7474f917803ba5e0c0f06ab
balaji-githubstore <balaji0017@gmail.com>
2021-08-19 12:13:19 +0530

    1212 comments

--- src/com/sg/seleniumadvance/RoyalTest.java
+++ src/com/sg/seleniumadvance/RoyalTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//b1
+//12:12
 public class RoyalTest {
 
 	public static void main(String[] args) {
